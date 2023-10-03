6c89433c0bf1efe965b12e6879cf8295eebdcbdb
leong <leong@DESKTOP-A61DB78>
2023-10-02 20:18:06 -0400

    Agregar archivos de proyecto.

---
+++ Practica 03 - Ejemplos de Arreglos/Practica_03___Ejemplos_de_Arreglos.py
@@ -0,0 +1,19 @@
+numeros = [10, 20, 30, 40, 50, 60]
+n=[3,4,5]
+
+
+c=['L','u', 'i', 's']
+
+s = "Mona Lisa"
+
+print("El arreglo de tipo entero de 6 elementos es: ") 
+print(numeros[0], numeros [1], numeros [2], numeros [3], numeros[4], numeros[5])
+
+print("El arreglo de tipo entero de 3 elementos es: ")
+print(n[0],n[1],[2])
+
+print("El arreglo de tipo char o letras de 4 elementos es: ")
+print(c[0],c[1],c[2],c[3])
+
+print("El arreglo de tipo char o letras de 9 elementos menos 1 vacia es:") 
+print(s[0],s[1],[2],[3],[5],[6],[7],[8])
